8f638ab5a5335ca41ea6a28768156c14b0891148
MartinHan <hanhan145@163.com>
2017-05-23 17:15:05 +0800

    fix bug

--- viewinject2find.py
+++ viewinject2find.py
@@ -33,12 +33,26 @@ if __name__ == '__main__':
             if find:
                 resid = find[0]
         else:
+            var_name = ''
+            type_name = ''
             pattern = re.compile(r'\s(\w+?);')
             find = re.findall(pattern ,line)
             if find:
                 var_name = find[len(find) - 1]
-                finds_res.append('%s = findViewById(%s);' % (var_name ,resid))
-                declarations.append(line)
+            else:
+                continue
+            if(re.match('^public|private|protected' ,line)):
+                type_pattern = re.compile(r'\s(\w+?)\s')
+                find_type = re.findall(type_pattern ,line)
+            else:
+                type_pattern = re.compile(r'(\w+)\s')
+                find_type = re.findall(type_pattern ,line)
+            type_name = find_type[0]
+            if len(type_name) == 0:
+                continue
+            print(find)
+            finds_res.append('%s = (%s)findViewById(%s);' % (var_name ,type_name ,resid))
+            declarations.append(line)
 
 
     print()
